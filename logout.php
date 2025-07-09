ccd221ed1d0a2b4709db147a9347c1a7f18373c4
DevLuis <157149686+luisdavid4564@users.noreply.github.com>
2025-07-08 21:41:11 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();        // Inicia sesión
-session_unset();        // Limpia variables de sesión
-session_destroy();      // Destruye sesión
-
-header("Location: login.php"); // Redirige al formulario de login
-exit();
-?>
